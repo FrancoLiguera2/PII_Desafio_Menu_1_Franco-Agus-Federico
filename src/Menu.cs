c3492bc4fb80ac30492b2a5079d436d63d74b3f3
FrancoLiguera2 <franco.liguera@correo.ucu.edu.uy>
2025-09-01 11:28:21 -0300

    Hice menu :)

--- src/Menu.cs
+++ src/Menu.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace Ucu.Poo.Restaurant
 {
@@ -9,5 +10,28 @@ namespace Ucu.Poo.Restaurant
     public class Menu
     {
         private List<Dish> dishes = new List<Dish>();
+
+        public void AddDish(Dish dish)
+        {
+            dishes.Add(dish);
+        }
+
+        public void RemoveDish(Dish dish)
+        {
+            dishes.Remove(dish);
+        }
+
+        public Dish GetDishByName(string name)
+        {
+            foreach (Dish dish in dishes)
+            {
+                if (dish.Name == name)
+                {
+                    return dish;
+                }
+            }
+
+            return null;
+        }
     }
 }
